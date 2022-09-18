9c00bc1a80f51e9e70bceee51fcf7621ad5e3612
piotrjarzabek <piotr.jarzabek@jupiter-integration.pl>
2022-09-18 16:25:05 +0200

    - router finished

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import Home from "./routes/home/home.component";
 import {Route, Routes} from "react-router-dom";
 import NavigationComponent from "./routes/navigation/navigation.component";
 
+const Shop = () => {
+    return <div>I am a shop</div>;
+}
+
 const App = () => {
     return (
         <Routes>
             <Route path="/" element={<NavigationComponent/>}>
                 <Route index element={<Home/>}/>
+                <Route path='shop' element={<Shop/>}/>
             </Route>
         </Routes>
     );
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,12 +1,24 @@
-import {Outlet} from "react-router-dom";
+import {Link, Outlet} from "react-router-dom";
+import {ReactComponent as Logo} from "../../assets/crown.svg"
+import './navigation.styles.scss';
+import {Fragment} from "react";
 
 const NavigationComponent = () => {
 
     return (
-        <div>
-            <h1>I am nav component</h1>
+        <Fragment>
+            <div className="navigation">
+                <Link className="logo-container" to='/'>
+                    <Logo className='logo'/>
+                </Link>
+                <div className='nav-links-container'>
+                    <Link className="nav-link" to='/shop'>
+                        Shop
+                    </Link>
+                </div>
+            </div>
             <Outlet/>
-        </div>
+        </Fragment>
     );
 }
 export default NavigationComponent;
